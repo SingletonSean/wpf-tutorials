1aaefce0ea0f888359f715368bf2116cce79542e
SingletonSean <bballheat6@gmail.com>
2021-04-19 20:58:56 -0400

    Implement animation

--- AnimationDemo/MainWindow.xaml.cs
+++ AnimationDemo/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,26 @@ namespace AnimationDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Duration _openCloseDuration = new Duration(TimeSpan.FromSeconds(0.5));
+
         public MainWindow()
         {
             InitializeComponent();
+
+            dropdownContent.Height = 0;
+        }
+
+        private void OpenDropdown(object sender, RoutedEventArgs e)
+        {
+            dropdownInnerContent.Measure(new Size(dropdownContent.MaxWidth, dropdownContent.MaxHeight));
+            DoubleAnimation heightAnimation = new DoubleAnimation(0, dropdownInnerContent.DesiredSize.Height, _openCloseDuration);
+            dropdownContent.BeginAnimation(HeightProperty, heightAnimation);
+        }
+
+        private void CloseDropdown(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation heightAnimation = new DoubleAnimation(0, _openCloseDuration);
+            dropdownContent.BeginAnimation(HeightProperty, heightAnimation);
         }
     }
 }
