4361d07d88a8fa1ade0533ee90471a0b2b05b641
SingletonSean <bballheat6@gmail.com>
2022-01-04 19:17:56 -0500

    Add ItemsControl demo

---
+++ ItemsControlDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MVVMEssentials.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ItemsControlDemo.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<ProductViewModel> _productViewModels;
+        public IEnumerable<ProductViewModel> ProductViewModels => _productViewModels;
+
+        public MainViewModel()
+        {
+            _productViewModels = new ObservableCollection<ProductViewModel>()
+            {
+                new ProductViewModel("T-Shirt", "A beautiful blue t-shirt.", 29.99),
+                new ProductViewModel("Sweatpants", "The greatest gray sweatpants.", 19.99),
+                new ProductViewModel("Shoes", "Some radical red shoes.", 179.99),
+                new ProductViewModel("Socks", "Just regular socks.", 4.99),
+                new ProductViewModel("Shorts", "A pair of shorts that you can use to wear around in, play sports, or record YouTube videos.", 19.99)
+            };
+        }
+    }
+}
---
+++ ItemsControlDemo/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MVVMEssentials.ViewModels;
+
+namespace ItemsControlDemo.ViewModels
+{
+    public class ProductViewModel : ViewModelBase
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public double Price { get; }
+
+        public ProductViewModel(string name, string description, double price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
